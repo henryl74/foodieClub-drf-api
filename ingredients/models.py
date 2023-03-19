8f9edfba75581c69e32bf3e4a5fd7ae7991a086d
Henry Leon <henryatusabes@gmail.com>
2023-03-19 20:50:45 +0000

    Create new Ingredients app, created models and migrate

---
+++ ingredients/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+# Create your models here.
+
+
+class Ingredients(models.Model):
+    """
+    Ingredients model, related to User and Post
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recipe = models.TextField()
+    method = models.TextField(null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content

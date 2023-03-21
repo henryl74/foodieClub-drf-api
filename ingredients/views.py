bc502392d8a7140680efa630a5e96590a52204d5
Henry Leon <henryatusabes@gmail.com>
2023-03-21 21:12:26 +0000

    Add Django filters backend to my Ingredients App

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Ingredients
 from .serializers import IngredientsSerializer, IngredientsDetailSerializer
@@ -13,6 +14,8 @@ class IngredientsList(generics.ListCreateAPIView):
     serializer_class = IngredientsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Ingredients.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
